523a1592e148b76001c6691c7541c93507f209db
Lorenzo Gravina <lorenzo.gravina@me.com>
2025-12-13 21:52:48 +0100

    implement subroutines and cond skips

--- src/chip8.c
+++ src/chip8.c
@@ -1,8 +1,8 @@
 #include "raylib.h"
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
 
 #define RAM_SIZE 4096
 #define STACK_DEPTH 16
@@ -32,7 +32,8 @@ BYTE delay_timer = 0;
 BYTE sound_timer = 0;
 
 // Initialize program counter and index register
-ADDRESS pc = ROM_START_ADDRESS; // Assuming that the ROM starts at this RAM addre
+ADDRESS pc =
+    ROM_START_ADDRESS; // Assuming that the ROM starts at this RAM addre
 ADDRESS I = 0;
 
 // Initializing registers
@@ -41,10 +42,8 @@ BYTE registers[16]; // 0-F
 // Creating the "pixel" arrays
 bool pixels[SCREEN_HEIGHT][SCREEN_WIDTH];
 
-int push_to_stack(ADDRESS address)
-{
-  if (stack_pointer >= STACK_DEPTH)
-  {
+int push_to_stack(ADDRESS address) {
+  if (stack_pointer >= STACK_DEPTH) {
     perror("Stack overflow.");
     return 1;
   }
@@ -55,10 +54,8 @@ int push_to_stack(ADDRESS address)
   return 0;
 }
 
-ADDRESS pop_from_stack()
-{
-  if (stack_pointer == 0)
-  {
+ADDRESS pop_from_stack() {
+  if (stack_pointer == 0) {
     perror("Stack underflow.");
     // TODO: It doesn't make sense to return exit_failure here. Find something
     // else. (e.g., they are both fine and valid addresses, both returns)
@@ -71,12 +68,10 @@ ADDRESS pop_from_stack()
   return address;
 }
 
-int load_rom_to_ram(char *filename)
-{
+int load_rom_to_ram(char *filename) {
   FILE *rom = fopen(filename, "rb");
 
-  if (!rom)
-  {
+  if (!rom) {
     perror("ROM not found.");
     return 1;
   }
@@ -86,10 +81,10 @@ int load_rom_to_ram(char *filename)
   int file_byte;
   int i = 0;
   // Reading the file one byte at a time into byte
-  while ((file_byte = fgetc(rom)) != EOF)
-  {
-    // TODO: Risky - if the ROM is too large, you'll go oob. do ram size - rom start addr to check
-    ram[pc + i] = file_byte; 
+  while ((file_byte = fgetc(rom)) != EOF) {
+    // TODO: Risky - if the ROM is too large, you'll go oob. do ram size - rom
+    // start addr to check
+    ram[pc + i] = file_byte;
     i++;
   }
 
@@ -97,11 +92,9 @@ int load_rom_to_ram(char *filename)
   return 0;
 }
 
-int dump_ram(void)
-{
+int dump_ram(void) {
   FILE *ram_dump = fopen("ram_dump.bin", "w");
-  if (!ram_dump)
-  {
+  if (!ram_dump) {
     perror("Failed creating the file ram_dump.bin");
     return 1;
   }
@@ -110,29 +103,23 @@ int dump_ram(void)
   printf("Dumped %zu bytes out of %d requested.", num_bytes_dumped, RAM_SIZE);
   fclose(ram_dump);
 
-  if (num_bytes_dumped == RAM_SIZE)
-  {
+  if (num_bytes_dumped == RAM_SIZE) {
     return 0;
-  }
-  else
-  {
+  } else {
     return 1;
   }
 }
 
-int reset_ram(void)
-{
+int reset_ram(void) {
   // Blank out memory
-  for (int i = 0; i < RAM_SIZE; i++)
-  {
+  for (int i = 0; i < RAM_SIZE; i++) {
     ram[i] = 0;
   }
 
   return 0;
 }
 
-void burn_font_to_ram(void)
-{
+void burn_font_to_ram(void) {
   int font_area_start_address = 0x050;
 
   int font[FONT_SIZE] = {
@@ -154,137 +141,136 @@ void burn_font_to_ram(void)
       0xF0, 0x80, 0xF0, 0x80, 0x80  // F
   };
 
-  for (int i = 0; i < FONT_SIZE; i++)
-  {
+  for (int i = 0; i < FONT_SIZE; i++) {
     ram[font_area_start_address + i] = font[i];
   }
 }
 
-void init_ram(void)
-{
+void init_ram(void) {
   // 0x000 - 0x1FF are reserved by the interpreter. 0x200+ are for the ROM.
   reset_ram();
   burn_font_to_ram();
-  // TODO: Many things to do here to init. Zero the timers, reset PC and I, registers, stack_pointer, pixels. basically make it so that you can run it while the emu is running.
+  // TODO: Many things to do here to init. Zero the timers, reset PC and I,
+  // registers, stack_pointer, pixels. basically make it so that you can run it
+  // while the emu is running.
 }
 
-void decrease_timers(void)
-{
-  if (sound_timer > 0)
-  {
+void decrease_timers(void) {
+  if (sound_timer > 0) {
     sound_timer--;
   }
 
-  if (delay_timer > 0)
-  {
+  if (delay_timer > 0) {
     delay_timer--;
   }
 }
 
-void paint_pixel_at_virtual_location(int x, int y)
-{
-  DrawRectangle(
-      x * SCREEN_MULTIPLIER,
-      y * SCREEN_MULTIPLIER,
-      SCREEN_MULTIPLIER - 1, // -1 to get a "grid"
-      SCREEN_MULTIPLIER - 1,
-      RAYWHITE);
+void paint_pixel_at_virtual_location(int x, int y) {
+  DrawRectangle(x * SCREEN_MULTIPLIER, y * SCREEN_MULTIPLIER,
+                SCREEN_MULTIPLIER - 1, // -1 to get a "grid"
+                SCREEN_MULTIPLIER - 1, RAYWHITE);
 }
 
-uint16_t fetch_instruction(void)
-{
+uint16_t fetch_instruction(void) {
   // Instructions are 16 bit
   uint16_t instruction_high = ram[pc];
   uint8_t instruction_low = ram[pc + 1];
 
   uint16_t instruction = (instruction_high << 8) | instruction_low;
 
-  // SIDE EFFECT: We also increase the PC by 2 to point it at the next instruction.
+  // SIDE EFFECT: We also increase the PC by 2 to point it at the next
+  // instruction.
   pc += 0x002;
   return instruction;
 }
 
-void clear_background(void)
-{
-  for (int i = 0; i < SCREEN_HEIGHT; i++)
-  {
-    for (int n = 0; n < SCREEN_WIDTH; n++)
-    {
+void clear_background(void) {
+  for (int i = 0; i < SCREEN_HEIGHT; i++) {
+    for (int n = 0; n < SCREEN_WIDTH; n++) {
       pixels[i][n] = false;
     }
   }
 }
 
-void draw_screen(void)
-{
+void draw_screen(void) {
   // Draws rectangles on screen based on the "pixels" matrix
-  for (int i = 0; i < SCREEN_HEIGHT; i++)
-  {
-    for (int n = 0; n < SCREEN_WIDTH; n++)
-    {
-      if (pixels[i][n])
-      {
-        DrawRectangle(
-            n * SCREEN_MULTIPLIER,
-            i * SCREEN_MULTIPLIER,
-            SCREEN_MULTIPLIER - 1,
-            SCREEN_MULTIPLIER - 1,
-            RAYWHITE);
-      }
-      else
-      {
-        DrawRectangle(
-            n * SCREEN_MULTIPLIER,
-            i * SCREEN_MULTIPLIER,
-            SCREEN_MULTIPLIER - 1,
-            SCREEN_MULTIPLIER - 1,
-            BLACK);
+  for (int i = 0; i < SCREEN_HEIGHT; i++) {
+    for (int n = 0; n < SCREEN_WIDTH; n++) {
+      if (pixels[i][n]) {
+        DrawRectangle(n * SCREEN_MULTIPLIER, i * SCREEN_MULTIPLIER,
+                      SCREEN_MULTIPLIER - 1, SCREEN_MULTIPLIER - 1, RAYWHITE);
+      } else {
+        DrawRectangle(n * SCREEN_MULTIPLIER, i * SCREEN_MULTIPLIER,
+                      SCREEN_MULTIPLIER - 1, SCREEN_MULTIPLIER - 1, BLACK);
       }
     }
   }
 }
 
-void decode_execute_instruction(uint16_t instruction)
-{
-  switch (instruction & 0xF000)
-  {
-  case 0x0000:
-  {
-    // Assuming 00E0 (for now), clear screen
-    clear_background();
-    break;
+void decode_execute_instruction(uint16_t instruction) {
+  switch (instruction & 0xF000) {
+  case 0x0000: {
+    // 00E0 - CLS
+    if (instruction == 0x00E0) {
+      clear_background();
+      break;
+    } else if (instruction == 0x00EE) {
+      /*
+      We have received the command to exit the subroutine we're in.
+      Let's pop the address from the stack and set the PC to it, so we can
+      resume execution.      
+      */
+      pc = pop_from_stack();
+    }
   }
 
-  case 0x1000:
-  {
+  case 0x1000: {
     // JMP (set pc to nnn)
-    pc = instruction & 0x0FFF;
+    pc = (instruction & 0x0FFF);
     break;
   }
 
-  case 0x6000:
-  {
+  case 0x2000: {
+    /*
+    The PC points at the instruction to execute.
+
+    In a normal jump, we move the PC to the new instruction, and that's it.
+
+    As this is a jump to subroutine though, this means that at some point we
+    will exit it and resume execution.
+
+    This is where the stack comes into play.
+
+    Before moving the PC to the new location, we push the current location of
+    the PC to the stack. Then, when we return from the subroutine (another
+    opcode), we resume execution as normal.
+
+    So here, just push the current PC to the stack and then jump to the
+    subroutine's address.
+    */
+    push_to_stack(ram[pc]);
+    pc = (instruction & 0x0FFF);
+  }
+
+  case 0x6000: {
     // Assuming 6XNN: Set register VX to NN
     registers[(instruction & 0x0F00) >> 8] = instruction & 0x00FF;
     break;
   }
 
-  case 0x7000:
-  {
+  case 0x7000: {
     // 7XNN: Add NN to VX
     registers[(instruction & 0x0F00) >> 8] += instruction & 0x00FF;
     break;
   }
 
-  case 0xA000:
-  {
+  case 0xA000: {
     // ANNN: Set I to NNN
     I = instruction & 0x0FFF;
     break;
   }
 
-  case 0xD000:
-  {
+  case 0xD000: {
     // DXYN: Draw sprite of N height from memory location at address I at X, Y
 
     // Get X and Y coords (if they overflow, they should wrap)
@@ -297,13 +283,11 @@ void decode_execute_instruction(uint16_t instruction)
     registers[0xF] = 0;
 
     // For each sprite row
-    for (int n = 0; n < sprite_height; n++)
-    {
+    for (int n = 0; n < sprite_height; n++) {
       uint8_t sprite_row = ram[I + n];
       // That's something like 11110000
 
-      for (int p = 0; p < 8; p++)
-      {
+      for (int p = 0; p < 8; p++) {
         // For each bit in the row, we get a single "pixel" (l to r)
 
         // Sliding mask to get one bit ("pixel") at a time
@@ -316,13 +300,13 @@ void decode_execute_instruction(uint16_t instruction)
         pixelToDraw: 10000000 >> (7-p) --> 00000001
         */
         bool pixel_to_draw = (sprite_row & mask) >> (7 - p);
-        if (pixel_to_draw)
-        {
+        if (pixel_to_draw) {
           /*
           This pixel needs to be drawn on screen.
           We flip the pixel on screen.
           */
-          pixels[(y + n) % SCREEN_HEIGHT][(x + p) % SCREEN_WIDTH] = !pixels[(y + n) % SCREEN_HEIGHT][(x + p) % SCREEN_WIDTH];
+          pixels[(y + n) % SCREEN_HEIGHT][(x + p) % SCREEN_WIDTH] =
+              !pixels[(y + n) % SCREEN_HEIGHT][(x + p) % SCREEN_WIDTH];
 
           // TODO: Set the collision register ONLY if a pixel goes from 1 -> 0
           // registers[0xF] = 1;
@@ -333,15 +317,13 @@ void decode_execute_instruction(uint16_t instruction)
     break;
   }
 
-  default:
-  {
+  default: {
     break;
   }
   }
 }
 
-int process_instruction(void)
-{
+int process_instruction(void) {
   /*
   TODO: Implement processing all instructions.
   This should fetch, decode, and execute the instruction.
@@ -353,15 +335,12 @@ int process_instruction(void)
   return 1;
 }
 
-int main(int argc, char *argv[])
-{
-  for (int i = 0; i < argc; i++)
-  {
+int main(int argc, char *argv[]) {
+  for (int i = 0; i < argc; i++) {
     printf("argv%d: %s\n", i, argv[i]);
   }
 
-  if (argc < 2)
-  {
+  if (argc < 2) {
     printf("Usage: chip8 <path_to_rom_file>\n");
     return 1;
   }
@@ -382,8 +361,7 @@ int main(int argc, char *argv[])
 
   dump_ram();
 
-  while (!WindowShouldClose())
-  {
+  while (!WindowShouldClose()) {
     BeginDrawing();
     current_frame_time = GetFrameTime();
     // DrawText(argv[1], 0, 0, 12, RAYWHITE);
@@ -391,8 +369,7 @@ int main(int argc, char *argv[])
     // Decrease timers once every 16ms
     timer_acc += current_frame_time;
 
-    while (timer_acc >= (1.0f / 60.0f))
-    {
+    while (timer_acc >= (1.0f / 60.0f)) {
       timer_acc -= (1.0f / 60.0f);
       decrease_timers();
     }
@@ -400,8 +377,7 @@ int main(int argc, char *argv[])
     // Process instructions at CPU_HZ
     cpu_acc += current_frame_time;
 
-    while (cpu_acc >= (1.0f / CPU_HZ))
-    {
+    while (cpu_acc >= (1.0f / CPU_HZ)) {
       cpu_acc -= (1.0f / CPU_HZ);
       process_instruction();
     }
